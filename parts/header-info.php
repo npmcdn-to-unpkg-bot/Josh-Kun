9d3c2ddac3af92e7ddc068b67d2a9c494c316dc7
Craig Fowler <craig@wearegiants.com>
2016-08-05 16:57:04 -0700

    email

--- parts/header-info.php
+++ parts/header-info.php
@@ -7,7 +7,7 @@
       <a class="ajax-link" href="<?php echo get_permalink(106); ?>"><?php echo get_the_title(106); ?></a>
     </div>
     <div class="fs-cell fs-lg-2 fs-md-1 fs-sm-1 text-right">
-      <a href="<?php echo get_permalink(); ?>" class="ss-gizmo ss-mail"></a>
+      <a href="mailto:joshkun@mac.com" class="ss-gizmo ss-mail"></a>
     </div>
   </div>
 </header>
